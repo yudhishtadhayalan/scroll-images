35073ceeecdf0280817beea14c0c7319944334c7
Mathivathani <mathi@torvistech.com>
2020-02-18 10:53:47 +0530

    Initial Commit

---
+++ Scroll Image/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scroll Image
+//
+//  Created by Mathivathani Murugesan on 18/02/20.
+//  Copyright © 2020 Yudhishta Dhayalan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Scroll ImageTests/Scroll_ImageTests.swift
@@ -0,0 +1,34 @@
+//
+//  Scroll_ImageTests.swift
+//  Scroll ImageTests
+//
+//  Created by Mathivathani Murugesan on 18/02/20.
+//  Copyright © 2020 Yudhishta Dhayalan. All rights reserved.
+//
+
+import XCTest
+@testable import Scroll_Image
+
+class Scroll_ImageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
