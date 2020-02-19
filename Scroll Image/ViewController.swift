a417744d70f0dd00c779cbf027e92b2d9e9a783e
Mathivathani <mathi@torvistech.com>
2020-02-20 00:28:33 +0530

    first commit

--- Scroll Image/ViewController.swift
+++ Scroll Image/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    //    @IBOutlet weak var imageViewOutlet: UIImageView!
+    @IBOutlet weak var scrollView : UIScrollView!
+    
+    var imagesss : [String] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for index in 0..<imagesss.count {
+            
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            
+            frame.size = scrollView.frame.size
+            
+            let imgView = UIImageView(frame: frame)
+            imgView.image = UIImage(named : imagesss[index])
+            self.scrollView.addSubview(imgView)
+            
+            
+            //            let imgView = UIImageView(frame: frame)
+            //            imageViewOutlet.image = UIImage(named : imagesss[index])
+            //            self.scrollView.addSubview(imageViewOutlet)
+            
+        }
+        
+        scrollView.contentSize = CGSize(width : scrollView.frame.size.width * CGFloat(imagesss.count), height: scrollView.frame.size.height)
+        scrollView.delegate = self
+        
     }
-
-
+    
+    
 }
 
